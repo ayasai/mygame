d4d0d5edb409e7e96bcf70a5e9887d39d2b80939
Aya <ayal1ghtt@mail.ru>
2024-05-08 13:27:15 +0500

    обработчик события нажатия на клавишу, с помощью которого вызывается функция прыжка

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 const scoreDisplay = document.getElementById("score");
 const highScoreDisplay = document.getElementById("high-score");
+
+document.addEventListener("keydown", jump);
