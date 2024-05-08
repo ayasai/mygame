79667ba5e69859e06182657b3cdfb1e6185ea9a6
Aya <ayal1ghtt@mail.ru>
2024-05-08 13:30:10 +0500

    функция определяет действие при нажатии на клавишу. в данном случае это прыжок. функция проверяет имеет ли элемент с айди dino  класс jump, если у элемента нет этого класса, то он добавляется. далее через 300 миллисекунд класс удаляется и элемент возвращается в исходное состояние

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const scoreDisplay = document.getElementById("score");
 const highScoreDisplay = document.getElementById("high-score");
 
 document.addEventListener("keydown", jump);
+function jump() {
+    if (!dino.classList.contains("jump")) {
+        dino.classList.add("jump");
+        setTimeout(() => {
+            dino.classList.remove("jump");
+        }, 300);
+    }
+}
