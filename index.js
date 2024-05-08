2555a49cc50eeac86130dfe76e8dfcb94074b778
Aya <ayal1ghtt@mail.ru>
2024-05-08 13:25:44 +0500

    инициализация переменных которые используются в игре

---
+++ index.js
@@ -0,0 +1,4 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+const scoreDisplay = document.getElementById("score");
+const highScoreDisplay = document.getElementById("high-score");
